94c301ea0947fbe6bc339991166ba3368f8b3812
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-12 22:14:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,4 +42,11 @@ class ApplicationController < Sinatra::Base
 
     erb :show
   end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+
+    erb :delete
+  end
 end
